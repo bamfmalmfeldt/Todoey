0ae3f63c7122ea6b79198ab83fc848c00c81039d
Michael Malmfeldt <dadfelt@gmail.com>
2018-03-06 06:52:30 -0500

    added to do list item showing up

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demorgon"]
+    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demorgon"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,6 +47,42 @@ class ToDoListViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    //MARK - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            // What will happen when user clicks add within the textbox
+            
+            print("Success!")
+            print(textField.text!)
+            
+            self.itemArray.append(textField.text!)
+            print(self.itemArray)
+            self.tableView.reloadData()
+          
+            
+          
+        }
+     
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            print(alertTextField.text!)
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
     
 }
 
